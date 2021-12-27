80df11ac1cc1fe0cd7a6d5e32afaa43b14dd4a4c
dotcommadotcom <byjiwoolee@gmail.com>
2021-12-27 15:32:59 +0900

    change build file settings

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,48 +1,61 @@
 plugins {
-    java
-    jacoco
+  java
+  jacoco
 }
 
 repositories {
-    mavenCentral()
+  mavenCentral()
 }
 
 dependencies {
-    implementation("junit:junit:4.12")
-    implementation("org.junit.jupiter:junit-jupiter:5.4.2")
-    testImplementation("org.junit.jupiter:junit-jupiter-api:5.7.0")
-    runtimeOnly("org.junit.jupiter:junit-jupiter-engine:5.7.0")
-    runtimeOnly("org.junit.platform:junit-platform-console:1.7.1")
+  implementation("junit:junit:4.12")
+  implementation("org.junit.jupiter:junit-jupiter:5.4.2")
+  testImplementation("org.junit.jupiter:junit-jupiter-api:5.7.0")
+  runtimeOnly("org.junit.jupiter:junit-jupiter-engine:5.7.0")
+  runtimeOnly("org.junit.platform:junit-platform-console:1.7.1")
 }
 
-tasks {
-    val flags = listOf("-Xlint:unchecked", "-Xlint:deprecation", "-Werror")
-
-    getByName<JavaCompile>("compileJava") {
-        options.compilerArgs = flags
-    }
-
-    getByName<JavaCompile>("compileTestJava") {
-        options.compilerArgs = flags
-    }
+tasks.test {
+  finalizedBy("jacocoTestReport")
+  doLast {
+    println("View code coverage at:")
+    println("file://$buildDir/reports/tests/test/index.html")
+  }
 }
-
-val test by tasks.getting(Test::class) {
-    useJUnitPlatform {}
-}
-
-tasks.withType<JacocoReport> {
-    afterEvaluate {
-        classDirectories.setFrom(files(classDirectories.files.map {
-            fileTree(it).apply {
-                exclude("*/preview/*")
-            }
-        }))
-    }
-}
-
-defaultTasks("clean", "test", "jacocoTestReport")
 
 jacoco {
-    toolVersion = "0.8.6"
+  toolVersion = "0.8.6"
 }
+
+defaultTasks("clean", "test")
+
+
+
+//tasks {
+//  val flags = listOf("-Xlint:unchecked", "-Xlint:deprecation", "-Werror")
+//
+//  getByName<JavaCompile>("compileJava") {
+//    options.compilerArgs = flags
+//  }
+//
+//  getByName<JavaCompile>("compileTestJava") {
+//    options.compilerArgs = flags
+//  }
+//}
+//
+//val test by tasks.getting(Test::class) {
+//  useJUnitPlatform {}
+//}
+//
+//tasks.withType<JacocoReport> {
+////  afterEvaluate {
+////    classDirectories.setFrom(files(classDirectories.files.map {
+////      fileTree(it).apply {
+////        exclude("*/preview/*")
+////      }
+////    }))
+////  }
+//}
+//
+
+//
