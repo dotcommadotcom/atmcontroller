ba05363102ec3059db2d72afaae8a1b8aed9bec0
dotcommadotcom <byjiwoolee@gmail.com>
2021-12-27 15:47:59 +0900

    Add ATMController class and test, canary test passed

---
+++ src/main/java/atmcontroller/ATMController.java
@@ -0,0 +1,4 @@
+package atmcontroller;
+
+public class ATMController {
+}
---
+++ src/test/java/atmcontroller/ATMControllerTest.java
@@ -0,0 +1,12 @@
+package atmcontroller;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class ATMControllerTest {
+  @Test
+  public void canaryTest() {
+    assertTrue(true);
+  }
+}
