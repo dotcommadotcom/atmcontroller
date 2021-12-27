e5b64023b8066f99ae6b43292461bb9c406c7a76
dotcommadotcom <byjiwoolee@gmail.com>
2021-12-27 16:27:33 +0900

    add ATMController class methods and unit test

--- src/main/java/atmcontroller/ATMController.java
+++ src/main/java/atmcontroller/ATMController.java
@@ -1,4 +1,33 @@
 package atmcontroller;
 
 public class ATMController {
+  private int balance;
+  private int pin;
+
+  public ATMController() {
+    balance = 0;
+    pin = 1004;
+  }
+
+  public ATMController(int _balance) {
+    balance = _balance;
+  }
+
+  public int showBalance() {
+    return balance;
+  }
+
+  public void deposit(int depositAmount) {
+    balance += depositAmount;
+  }
+
+  public void withdraw(int withdrawAmount) {
+    if (withdrawAmount <= balance) {
+      balance -= withdrawAmount;
+    }
+  }
+
+  public boolean isPinVerified(int pinEntered) {
+    return pin == pinEntered;
+  }
 }
--- src/test/java/atmcontroller/ATMControllerTest.java
+++ src/test/java/atmcontroller/ATMControllerTest.java
@@ -1,12 +1,81 @@
 package atmcontroller;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.*;
 
 public class ATMControllerTest {
+  private ATMController atm;
+
+  @BeforeEach
+  public void setUp() {
+    atm = new ATMController();
+  }
+
   @Test
   public void canaryTest() {
     assertTrue(true);
+  }
+
+  @Test
+  public void showBalanceIsZero() {
+    assertEquals(atm.showBalance(), 0);
+  }
+
+  @Test
+  public void showBalanceIsTen() {
+    ATMController atm = new ATMController(10);
+
+    assertEquals(atm.showBalance(), 10);
+  }
+
+  @Test
+  public void depositTenDollars() {
+    atm.deposit(10);
+
+    assertEquals(atm.showBalance(), 10);
+  }
+
+  @Test
+  public void depositTwentyDollars() {
+    atm.deposit(20);
+
+    assertEquals(atm.showBalance(), 20);
+  }
+
+  @Test
+  public void withdrawTenDollarsGetZeroBalance() {
+    atm.deposit(10);
+
+    atm.withdraw(10);
+
+    assertEquals(atm.showBalance(), 0);
+  }
+
+  @Test
+  public void withdrawTenDollarsGetTenBalance() {
+    atm.deposit(20);
+
+    atm.withdraw(10);
+
+    assertEquals(atm.showBalance(), 10);
+  }
+
+  @Test
+  public void withdraw100ForZeroBalance() {
+    atm.withdraw(100);
+
+    assertEquals(atm.showBalance(), 0);
+  }
+
+  @Test
+  public void verifyPinNumberIsTrue() {
+    assertTrue(atm.isPinVerified(1004));
+  }
+
+  @Test
+  public void verifyPinNumber() {
+    assertFalse(atm.isPinVerified(1005));
   }
 }
